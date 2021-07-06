4a274f6f9036250cbd59dc13856d9e0d3253bcd6
Anne Porter <a.porter9212@gmail.com>
2021-07-06 16:14:33 -0400

    added films

--- ghibli.js
+++ ghibli.js
@@ -2,8 +2,9 @@ const baseURL = 'https://ghibliapi.herokuapp.com'
 
 let studioGhibliPeople = document.querySelector('ul');
 let showChar = document.querySelector('.button');
-
+let studioGhibliFilms = document.querySelector('.films');
 showChar.addEventListener('click', displayPic);
+showChar.addEventListener('click', displayFilm);
 
 
 function displayPic(event) {
@@ -13,8 +14,8 @@ function displayPic(event) {
     })
     .then (function(json) {
         let people = json;
-        console.log(people);
-        console.log(people[0].name)
+        // console.log(people);
+        // console.log(people[0].name)
         for (p of people) {
             let listItem = document.createElement('li');
             listItem.innerHTML = '<p>' + p.name + '</p>';
@@ -22,4 +23,23 @@ function displayPic(event) {
         };   
     });
 }
+
+function displayFilm(event) {
+    fetch ('https://ghibliapi.herokuapp.com/films')
+    .then (function(response) {
+        return response.json ();
+    })
+    .then (function(json) {
+        let films = json;
+        console.log(films);
+        console.log(films[0].title)
+        for (f of films) {
+            let filmList = document.createElement('li');
+            filmList.innerHTML = '<p>' + f.title + '</p>';
+            studioGhibliFilms.appendChild(filmList);
+        };   
+    });
+}
+
+
 

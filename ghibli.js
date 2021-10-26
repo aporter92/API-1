2aceded730e6156cfd20659f3bebb29dbf7bca7c
Anne Porter <a.porter9212@gmail.com>
2021-10-26 16:17:31 -0400

    added images to film list

--- ghibli.js
+++ ghibli.js
@@ -28,17 +28,21 @@ fetch ('https://ghibliapi.herokuapp.com/people')
     return result.json()
 })
 .then (function(json) {
-    console.log(json);
+    // console.log(json);
     displayPic(json);
 })
 
 function displayPic(json) {
         let people = json;
-        // console.log(people);
-        // console.log(people[0].name)
+        console.log(people[0].name)
+        console.log(people)
         for (p of people) {
             let listItem = document.createElement('li');
-            listItem.innerHTML = '<p>' + p.name + '</p>';
+            // let img = document.createElement('img');
+            // img.setAttribute("src", p.films);
+            listItem.innerHTML = '<p>' + p.name + 
+            '<p>' + "age: " + p.age + '</p>' +
+            '<p>' + "eye color: " + p.eye_color + '<p>';
             studioGhibliPeople.appendChild(listItem);
         };   
 }
@@ -54,12 +58,19 @@ fetch ('https://ghibliapi.herokuapp.com/films')
 
 function displayFilm(json) {
         let films = json;
-        console.log(films);
-        console.log(films[0].title)
+        
         for (f of films) {
             let filmList = document.createElement('li');
+            let image = document.createElement("img");
+            image.setAttribute("src", f.movie_banner)
             filmList.innerHTML = '<p>' + f.title + '</p>';
-            studioGhibliFilms.appendChild(filmList);
+            
+            // var documentFragment = document.createDocumentFragment();
+            // documentFragment.appendChild(filmList);
+
+            studioGhibliFilms.appendChild(filmList)
+            studioGhibliFilms.appendChild(image);
+            
         };  
 }
 
